a1c6007cc17429a05402c52e804e3908095d4cde
José Vilela <jvilela@beeverycreative.com>
2022-03-10 14:35:49 +0000

    adding header component amd links to home, shop page and contat

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {BrowserRouter, Route, Routes  } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component';
 
 
 function App() {
   
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
