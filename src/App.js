f0cf24e8d12c2d2fa0495ac1c1299c17e4113891
José Vilela <jvilela@beeverycreative.com>
2022-03-10 11:48:24 +0000

    update our menu-items to navigate withRouter HOC, then changed to useNavigate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 
+import {BrowserRouter, Route, Routes  } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
+  
   return (
-    <div >
-      <HomePage></HomePage>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,21 +1,31 @@
 import React from "react";
+//import { withRouter} from 'react-router-dom';
+
+import { useNavigate } from "react-router-dom";
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}
-    >
-        <div 
-            className="background-image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className="content">
-            <h1 className="title">{ title }</h1>
-            <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl }) => {
+    const navigate = useNavigate();
+
+    return(
+        //<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}
+        
+        <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}
+        >
+            <div 
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className="content">
+                <h1 className="title">{ title }</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default MenuItem;
+//export default withRouter(MenuItem);
